17608b2ed941f5256ae50f03052140b261bb5eeb
Duck <148314626+Rroq1@users.noreply.github.com>
2024-09-29 10:20:47 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,34 +1,53 @@
-
 import random
+from tkinter import *
 
-# 1 : Je pose la question à l'utilisateur
-question = int(input("Combien de caractères voulez-vous dans votre mot de passe ? "))
+# 1 : Création de la fenêtre Tkinter
+fenetre = Tk()
+fenetre.title("Générateur de mot de passe")
+fenetre.minsize(500, 300)
 
-# 2 : Je définis différentes catégories de caractères
-lettres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-nombres = '0123456789'
-symboles = '!#$%&()*+'
+# 2 : Titre
+titre = Label(fenetre, text="Combien de caractères voulez-vous dans votre mot de passe ?")
+titre.pack()
 
-# 3 : J'aoute toutes les catégories ensemble
-ensemble_des_caracteres=lettres+nombres+symboles
+# 3 : Champ pour entrer la longueur du mot de passe
+expression = StringVar()
+entree = Entry(fenetre, textvariable=expression, width=30)
+entree.pack(pady=10)
 
+# 4 : Le label pour afficher le mot de passe généré
+resultat = StringVar()
+sortie = Entry(fenetre, textvariable=resultat, width=40)
+sortie.pack()
 
-# 4 : J'assure qu'il y a au moins chaque type de catégorie dans le mot de passe
-mot_de_passe = [
-    random.choice(lettres),
-    random.choice(nombres),
-    random.choice(symboles)
-]
+# 5 : Fonction pour générer le mot de passe utilisant la première version du générateur
+def generer_mot_de_passe():
+    try:
+        longueur = int(expression.get())
+        
+        lettres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        nombres = '0123456789'
+        symboles = '!#$%&()*+'
+        
+        ensemble_des_caracteres = lettres + nombres + symboles
+        
+        mot_de_passe = [
+            random.choice(lettres),
+            random.choice(nombres),
+            random.choice(symboles)
+        ]
 
-# 5 : Je remplis le reste du mot de passe avec des caractères aléatoire
-for n in range(question - 3):
-    mot_de_passe.append(random.choice(ensemble_des_caracteres))
+        for _ in range(longueur - 3):
+            mot_de_passe.append(random.choice(ensemble_des_caracteres))
 
-# 6 :  Je mélange l'ordre des caractères
-random.shuffle(mot_de_passe)
+        random.shuffle(mot_de_passe)
 
-# 7 : Je transforme mon mot de passe en chaîne de caractère
-mot_de_passe_final = ''.join(mot_de_passe)
+        mot_de_passe_final = ''.join(mot_de_passe)
+        resultat.set(mot_de_passe_final)
+    except ValueError:
+        resultat.set("Veuillez entrer un nombre valide.")
 
-# 8 : J'affiche le mot de passe dans le terminal
-print("Votre mot de passe généré est :", mot_de_passe_final)
+bouton = Button(fenetre, text="Générer", command=generer_mot_de_passe)
+bouton.pack(pady=10)
+
+fenetre.mainloop()
